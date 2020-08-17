b6a33e017ce7e854bca1bb3c912c8af18c0d508f
Colin Marsch <colinmarsch@google.com>
2020-08-16 23:51:54 -0500

    AOSP/DeskClock - Add Kotlin code for VerticalViewPager
    Test: manual - Tested the DeskClock UI. As well tests were ran as
    follows
    
    $ source build/envsetup.sh
    $ lunch aosp_sargo-userdebug
    $ make DeskClock
    $ adb install out/target/product/sargo/product/app/DeskClock/DeskClock.apk
    $ atest DeskClockTests
    
    BUG: 157255731
    Change-Id: Icd49eb11e4c6c97d296f661de35635de6a833d04

--- src/com/android/deskclock/VerticalViewPager.kt
+++ src/com/android/deskclock/VerticalViewPager.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,94 +14,88 @@
  * limitations under the License.
  */
 
-package com.android.deskclock;
+package com.android.deskclock
 
-import android.content.Context;
-import androidx.viewpager.widget.ViewPager;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.viewpager.widget.ViewPager
 
-public class VerticalViewPager extends ViewPager {
-
-    public VerticalViewPager(Context context) {
-        this(context, null);
-    }
-
-    public VerticalViewPager(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
+class VerticalViewPager @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : ViewPager(context, attrs) {
+    init {
+        init()
     }
 
     /**
-     * @return {@code false} since a vertical view pager can never be scrolled horizontally
+     * @return `false` since a vertical view pager can never be scrolled horizontally
      */
-    @Override
-    public boolean canScrollHorizontally(int direction) {
-        return false;
-    }
+    override fun canScrollHorizontally(direction: Int): Boolean = false
 
     /**
-     * @return {@code true} iff a normal view pager would support horizontal scrolling at this time
+     * @return `true` iff a normal view pager would support horizontal scrolling at this time
      */
-    @Override
-    public boolean canScrollVertically(int direction) {
-        return super.canScrollHorizontally(direction);
+    override fun canScrollVertically(direction: Int): Boolean {
+        return super.canScrollHorizontally(direction)
     }
 
-    private void init() {
+    private fun init() {
         // Make page transit vertical
-        setPageTransformer(true, new VerticalPageTransformer());
+        setPageTransformer(true, VerticalPageTransformer())
         // Get rid of the overscroll drawing that happens on the left and right (the ripple)
-        setOverScrollMode(View.OVER_SCROLL_NEVER);
+        setOverScrollMode(View.OVER_SCROLL_NEVER)
     }
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        final boolean toIntercept = super.onInterceptTouchEvent(flipXY(ev));
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        val toIntercept: Boolean = super.onInterceptTouchEvent(flipXY(ev))
         // Return MotionEvent to normal
-        flipXY(ev);
-        return toIntercept;
+        flipXY(ev)
+        return toIntercept
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        final boolean toHandle = super.onTouchEvent(flipXY(ev));
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        val toHandle: Boolean = super.onTouchEvent(flipXY(ev))
         // Return MotionEvent to normal
-        flipXY(ev);
-        return toHandle;
+        flipXY(ev)
+        return toHandle
     }
 
-    private MotionEvent flipXY(MotionEvent ev) {
-        final float width = getWidth();
-        final float height = getHeight();
+    private fun flipXY(ev: MotionEvent): MotionEvent {
+        val width: Float = width.toFloat()
+        val height: Float = height.toFloat()
 
-        final float x = (ev.getY() / height) * width;
-        final float y = (ev.getX() / width) * height;
+        val x = ev.y / height * width
+        val y = ev.x / width * height
 
-        ev.setLocation(x, y);
+        ev.setLocation(x, y)
 
-        return ev;
+        return ev
     }
 
-    private static final class VerticalPageTransformer implements ViewPager.PageTransformer {
-        @Override
-        public void transformPage(View view, float position) {
-            final int pageWidth = view.getWidth();
-            final int pageHeight = view.getHeight();
-            if (position < -1) {
-                // This page is way off-screen to the left.
-                view.setAlpha(0);
-            } else if (position <= 1) {
-                view.setAlpha(1);
-                // Counteract the default slide transition
-                view.setTranslationX(pageWidth * -position);
-                // set Y position to swipe in from top
-                float yPosition = position * pageHeight;
-                view.setTranslationY(yPosition);
-            } else {
-                // This page is way off-screen to the right.
-                view.setAlpha(0);
+    private class VerticalPageTransformer : ViewPager.PageTransformer {
+        override fun transformPage(view: View, position: Float) {
+            val pageWidth = view.width
+            val pageHeight = view.height
+            when {
+                position < -1 -> {
+                    // This page is way off-screen to the left.
+                    view.alpha = 0f
+                }
+                position <= 1 -> {
+                    view.alpha = 1f
+                    // Counteract the default slide transition
+                    view.translationX = pageWidth * -position
+                    // set Y position to swipe in from top
+                    val yPosition = position * pageHeight
+                    view.translationY = yPosition
+                }
+                else -> {
+                    // This page is way off-screen to the right.
+                    view.alpha = 0f
+                }
             }
         }
     }
